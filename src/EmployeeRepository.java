49810b868e4f936508d5690b456e1bd58e021cb0
Matheus Bezerra Luna <147543651+MatheusLuna37@users.noreply.github.com>
2024-09-23 16:22:35 -0300

    fixes bug

--- src/EmployeeRepository.java
+++ src/EmployeeRepository.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class EmployeeRepository extends BaseRepository<Employee> {
     public ArrayList<Employee> find_by_position(String position) {
-        ArrayList<Employee> result = null;
+        ArrayList<Employee> result = new ArrayList<>();
         for (Employee employee : this.data) {
             if (employee.getPosition().equals(position)) {
                 result.add(employee);
